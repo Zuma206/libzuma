d46bd401436c391bc278d164a6d0d8cc29a51e11
Zuma <zuma@zuma.eu.org>
2026-07-04 18:01:45 +0100

    Added a test for heap allocate

--- test.c
+++ test.c
@@ -1,5 +1,10 @@
 #include "zuma.h"
+#include <stdio.h>
 
 int main() {
+  int *count = allocate(heap, int);
+  *count = 0;
+  (*count)++;
+  printf("count (expected 1) = %d\n", *count);
   panic("This is a planned panic! Program should now exit with status 1\n");
 }
